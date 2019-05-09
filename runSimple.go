90401bf3b89abc48f47f82fdf374eaaf5df02b36
Chris Mark <chrismarkou92@gmail.com>
2019-05-09 16:52:59 +0300

    Add Commit resctrl
    Signed-off-by: Chris Mark <chrismarkou92@gmail.com>

--- runSimple.go
+++ runSimple.go
@@ -6,13 +6,14 @@ import (
 )
 
 func main() {
+	resctrl.RemoveTasks()
 	resGroups := resctrl.GetResAssociation()
 	fmt.Println(resGroups)
 	rs := resctrl.NewResAssociation()
-	rs.CPUs = "2"
+	rs.CPUs = "3"
 	// mask = 00010000000
 	// echo "L3:0=80;1=7ff" > COS2/schemata
 	group := []resctrl.CacheCos{{0, "80"}, {1, "7ff"}}
 	rs.Schemata["L3"] = group
-	resctrl.Commit(rs, "COS2")
+	resctrl.Commit(rs, "COS4")
 }
